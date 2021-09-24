c185541447bd8434d26dbc12ed2caaf81129d249
devgl96 <georgelucas.dev@gmail.com>
2021-09-24 16:00:05 -0300

    Complete Lesson - CoindexCLI

---
+++ bin/coindex.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const pkg = require("../package.json");
+
+program
+    .version(pkg.version)
+    .command("key", "Manage API Key -- https://nomics.com")
+    .command("check", "Check Coin Price Info")
+    .parse(process.argv);
+
+// console.log("Hello from coindex.js");
+// console.log(process.argv[2]); // Get the arguments
---
+++ commands/check.js
@@ -0,0 +1,24 @@
+const KeyManager = require("../lib/KeyManager");
+const CryptoAPI = require("../lib/CryptoAPI");
+
+const check = {
+    async price(cmd) {
+        // console.log("Hello from price");
+        // console.log(cmd.coin, cmd.cur);
+
+        try {
+            const keyManager = new KeyManager();
+            const key = keyManager.getKey(); 
+
+            const api = new CryptoAPI(key);
+
+            const priceOutputData = await api.getPriceData(cmd.coin, cmd.cur);
+
+            console.log(priceOutputData);
+        } catch (err) {
+            console.error(err.message.red);
+        }
+    }
+};
+
+module.exports = check;
